697e6ead5c97c90bd124ce80b7bd6d8b4b9ba87e
Antonius <47992271+Antoine1983@users.noreply.github.com>
2024-11-30 19:06:27 +0000

    Update core.py

--- core.py
+++ core.py
@@ -72,8 +72,6 @@ def get_markdown(random_movie, ratings, tmdb_api_key):
 
 ![{random_movie['primaryTitle']}]({poster_path})
 
-**Information courtesy of IMDb (https://www.imdb.com) and TMDB (https://www.themoviedb.org). Used with permission.**
-
 '''
     
     return markdown
