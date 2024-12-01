2b2725b9ddc67abf4fbe78517344eab58677f913
Antonius <47992271+Antoine1983@users.noreply.github.com>
2024-12-01 11:29:10 +0000

    Light improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,13 @@ st.set_page_config(page_title='What should I watch?')
                    
 st.title('What should I watch?')
 
-@st.cache_data
+@st.cache_data(persist="disk")
 def init_data():
     return core.load_good_movies()
 
 movies, ratings = init_data()
 
-@st.cache_data(ttl="1d")
+@st.cache_data(ttl="1h")
 def randomize_sequence():
     return core.randomize_sequence(len(movies.index))
 
@@ -40,7 +40,7 @@ with st.spinner('Wait for it...'):
 
 st.write('Random Movie Roulette: randomly choose a good movie to watch !') 
 
-if st.button("Try again ?"):
+if st.button("Try again ?", type="primary"):
     choose_next()
     st.rerun()
 
