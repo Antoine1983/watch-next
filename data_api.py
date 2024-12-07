0a120d5ad785997f7b221710289d62c3c35d46f4
Antonius <47992271+Antoine1983@users.noreply.github.com>
2024-12-07 17:52:59 +0000

    Minor improvement

--- data_api.py
+++ data_api.py
@@ -6,11 +6,6 @@ data_folder = './data/'
 def load_good_movies():
     return pandas.read_parquet(f'{data_folder}good_movies.parquet')
 
-def randomize_sequence(total_movies):
-    sequence = list(range(0, total_movies))
-    random.shuffle(sequence)
-    return sequence
-
 def get_poster_url(movie_info):
     img_url = ''
     if 'poster_path' in movie_info:
