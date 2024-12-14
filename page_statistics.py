0040d824471a67b1600abe8ed3c6e920b15c357f
Antonius <47992271+Antoine1983@users.noreply.github.com>
2024-12-14 18:24:13 +0000

    Minor improvement

--- page_statistics.py
+++ page_statistics.py
@@ -11,20 +11,17 @@ movies = load_good_movies()
 st.title("Statistics")
 
 st.header("Distribution per average rating")
-
 fig1, ax1 = plt.subplots()
 ax1.hist(movies['averageRating'], bins=12)
 st.pyplot(fig1)
 
 st.header("Distribution per year")
-
 fig2, ax2 = plt.subplots()
 ax2.hist(movies['startYear'], bins=12)
 st.pyplot(fig2)
 
 
 st.header("Distribution per region")
-
 fig3, ax3 = plt.subplots()
 c = movies['country_region'].value_counts()
 ax3.pie(c, labels=c.index)
