06da35753351e40bbc0fabb2896e494f40cef489
Antoine Viellard <antoine.viellard@gmail.com>
2025-03-23 15:44:58 +0000

    Minor UI update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pg = st.navigation(
     [
-        st.Page("page_roulette.py", title="Movie Roulette", icon="🔀"),
         st.Page("page_top_movies.py", title="Top movies", icon="🏅"),
+        st.Page("page_roulette.py", title="Movie Roulette", icon="🔀"),
         st.Page("page_statistics.py", title="Stats", icon="📈")
     ]
 )
